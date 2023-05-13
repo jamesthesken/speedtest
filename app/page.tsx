5e4a7a1f4788086d0b28107949c15100ec7c4451
jamesthesken <jthesken@hawaii.edu>
2023-05-13 07:21:34 -1000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,89 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import SpeedTest from "@cloudflare/speedtest";
 
 export default function Home() {
+  const [speedTestResults, setSpeedTestResults] = useState<any>();
+
+  const config = {
+    autoStart: false,
+    measurements: [
+      { type: "latency", numPackets: 1 }, // initial latency estimation
+      { type: "download", bytes: 1e5, count: 1, bypassMinDuration: true }, // initial download estimation
+      { type: "latency", numPackets: 20 },
+      { type: "download", bytes: 1e5, count: 9 },
+      { type: "download", bytes: 1e6, count: 8 },
+      { type: "upload", bytes: 1e5, count: 8 },
+      { type: "packetLoss", numPackets: 1e3, responsesWaitTime: 3000 },
+      { type: "upload", bytes: 1e6, count: 6 },
+      { type: "download", bytes: 1e7, count: 6 },
+      { type: "upload", bytes: 1e7, count: 4 },
+      { type: "download", bytes: 2.5e7, count: 4 },
+      { type: "upload", bytes: 2.5e7, count: 4 },
+      { type: "download", bytes: 1e8, count: 3 },
+      { type: "upload", bytes: 5e7, count: 3 },
+      { type: "download", bytes: 2.5e8, count: 2 },
+    ],
+  };
+
+  const safeFetch = async (url: any, options = {}) => {
+    try {
+      const request = await fetch(url, options);
+      const json = JSON.parse(await request.text());
+      const headers = await request.headers;
+      return { json, headers };
+    } catch (_) {
+      return {};
+    }
+  };
+
+  const runSpeedTest = async () => {
+    const ua = { user_agent: window.navigator.userAgent };
+    const meta = (await safeFetch("https://speed.cloudflare.com/meta")).json;
+    const { utc_datetime, unixtime } = (
+      await safeFetch("https://worldtimeapi.org/api/timezone/Etc/UTC")
+    ).json;
+    const ts = { epoch: unixtime, dateTime: utc_datetime };
+
+    const engine = new SpeedTest(config as any);
+
+    engine.onResultsChange = (results) => {
+      const summary = engine.results.getSummary();
+      const scores = engine.results.getScores();
+      setSpeedTestResults({ ...scores, ...summary, ...meta, ...ts, ...ua });
+    };
+
+    engine.onFinish = (results) => {
+      const summary = results.getSummary();
+      const scores = results.getScores();
+      setSpeedTestResults({ ...scores, ...summary, ...meta, ...ts, ...ua });
+      console.log({ ...scores, ...summary, ...meta, ...ts, ...ua });
+      const finishedElement = document.createElement("div");
+      finishedElement.id = "speedtest-finished";
+      document.body.appendChild(finishedElement);
+    };
+
+    console.log("running");
+
+    engine.play();
+  };
+
+  useEffect(() => {
+    runSpeedTest();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {speedTestResults && (
+          <div>
+            <h1>Speed Test Results:</h1>
+            <pre>{JSON.stringify(speedTestResults, null, 2)}</pre>
+            <p>Streaming Points: {speedTestResults.download}</p>
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }
