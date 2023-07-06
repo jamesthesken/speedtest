c9efeb9c5d569a248c136cca5a3ad94da1194105
jamesthesken <jthesken@hawaii.edu>
2023-07-05 15:25:55 -1000

    feat: tooltip regarding network quality scores

--- components/results.tsx
+++ components/results.tsx
@@ -1,6 +1,7 @@
 "use-client";
 import { useEffect } from "react";
 import { SpeedTestResults } from "@/app/page";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 function formatBytes(bits: number, decimals = 2) {
   if (!+bits) return "0 Bits";
@@ -10,7 +11,6 @@ function formatBytes(bits: number, decimals = 2) {
   const sizes = ["Bits", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
   const i = Math.floor(Math.log(bits) / Math.log(k));
-  console.log(`${parseFloat((bits / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`);
 
   return `${parseFloat((bits / Math.pow(k, i)).toFixed(dm))}`;
 }
@@ -95,20 +95,36 @@ export default function Results(speedTestResults: SpeedTestResults) {
               </p>
             </div>
           </div>
-
+          <div className="flex flex-row bg-slate-900 p-8">
+            <h1>Network Quality Scores</h1>
+            <div className="group relative w-max pl-8">
+              <InformationCircleIcon className="text-gray-100 h-5 w-5" />
+              <span className="pointer-events-none bg-gray-700 w-96 absolute -top-7 left-0 opacity-0 transition-opacity group-hover:opacity-100 rounded-lg p-8">
+                Aggregated Internet Measurement (AIM) helps you understand your
+                Internet quality to identify scenarios that your Internet
+                connection is good or bad for. Typically, an Internet speed test
+                provides you with upload and download speeds, which may not
+                always provide a holistic view of your Internet quality.
+              </span>
+            </div>
+          </div>
           <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-3">
             <div className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-              <p className="text-sm font-medium leading-6 text-gray-400">
-                {speedTestResults?.streaming ? (
-                  <span>{speedTestResults?.streaming.points} points</span>
-                ) : (
-                  <span>--</span>
-                )}
-              </p>
+              <div className="flex flex-row justify-between">
+                <p className="text-sm font-medium leading-6 text-gray-400">
+                  {speedTestResults?.streaming ? (
+                    <span>{speedTestResults?.streaming.points} points</span>
+                  ) : (
+                    <span>--</span>
+                  )}
+                </p>
+                <div></div>
+              </div>
+
               <p className="mt-2 flex items-baseline gap-x-2">
                 {speedTestResults?.streaming ? (
                   <span className="text-2xl font-semibold tracking-tight text-white">
-                    {speedTestResults?.streaming.classificationName} for
+                    {speedTestResults?.streaming.classificationName} for video
                     streaming
                   </span>
                 ) : (
@@ -149,7 +165,8 @@ export default function Results(speedTestResults: SpeedTestResults) {
               <p className="mt-2 flex items-baseline gap-x-2">
                 {speedTestResults?.rtc ? (
                   <span className="text-2xl font-semibold tracking-tight text-white">
-                    {speedTestResults?.rtc.classificationName} for live comms
+                    {speedTestResults?.rtc.classificationName} for video
+                    chatting
                   </span>
                 ) : (
                   <span className="text-4xl font-semibold tracking-tight text-white">
@@ -165,7 +182,7 @@ export default function Results(speedTestResults: SpeedTestResults) {
       <br></br>
 
       <div className="mx-auto max-w-7xl">
-        <h1>Details</h1>
+        <h1 className="pb-2">Details</h1>
         <div className="grid grid-rows-1 divide-y-2 divide-gray-200 gap-px bg-white/5 p-4 items-center">
           <div className="grid grid-cols-1 gap-px sm:grid-cols-2 lg:grid-cols-2">
             <p>Downloaded Latency</p>
@@ -194,7 +211,7 @@ export default function Results(speedTestResults: SpeedTestResults) {
           <div className="grid grid-cols-1 gap-px sm:grid-cols-2 lg:grid-cols-2">
             <p>Packet Loss</p>
             <p className="mt-2 flex items-baseline gap-x-2">
-              {speedTestResults?.packetLoss} ms
+              {speedTestResults?.packetLoss}
             </p>
           </div>
           <div className="grid grid-cols-1 gap-px sm:grid-cols-2 lg:grid-cols-2">
@@ -203,14 +220,6 @@ export default function Results(speedTestResults: SpeedTestResults) {
               {speedTestResults?.dateTime}
             </p>
           </div>
-          {/* {Object.keys(speedTestResults)?.map((data, i) => (
-            <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-2">
-              <p>{data}</p>
-              <p className="mt-2 flex items-baseline gap-x-2">
-                {speedTestResults?.}
-              </p>
-            </div>
-          ))} */}
         </div>
       </div>
     </div>
