02256d90832ba2501b8cfe08bd0ace743652ea2c
jamesthesken <jthesken@hawaii.edu>
2023-07-06 15:00:14 -1000

    fix: email is required

--- components/contact.tsx
+++ components/contact.tsx
@@ -95,6 +95,7 @@ export default function Contact() {
                 id="email"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                required
               />
             </div>
           </div>
