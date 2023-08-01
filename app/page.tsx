f84e89dbc52e23568abc8158938230559e69f030
jacob <jacobkrees3@gmail.com>
2023-08-01 11:27:14 -1000

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
 
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-100 sm:text-4xl">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-100 sm:text-6xl">
               Kauai Speed Test
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-200">
@@ -202,7 +202,7 @@ export default function Home() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                href="#survey"
+                href="#speedtest"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
@@ -226,7 +226,7 @@ export default function Home() {
   
 {/* ----------Survey---------- */}
 
-      <div className="items-center isolate px-6 py-24 sm:py-32 lg:px-8" id="survey">
+      {/* <div className="items-center isolate px-6 py-24 sm:py-32 lg:px-8" id="survey">
         <Survey setSurveyInfo={setSurveyInfo} setSubmitted={setSubmitted}/>
         {submitted &&
           <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -238,7 +238,7 @@ export default function Home() {
             </Link>
           </div>
         }
-      </div>
+      </div> */}
 
 {/* ----------Speed Test Intro---------- */}
 
@@ -402,7 +402,7 @@ export default function Home() {
       </div>
       <Modal open={open} setOpen={setOpen} />
       <Contact />
-      <Link href="/data">See the data</Link>
+      {/* <Link href="/data">See the data</Link> */}
       <Footer />
     </div>
   );
